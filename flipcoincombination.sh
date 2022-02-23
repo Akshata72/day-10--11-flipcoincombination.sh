5a122f192671a3810b0d8549bdf9fde2c585af10
Akshata72 <sableakshata880@gmail.com>
2022-02-23 15:52:57 +0530

    Added:UC2 logic

--- flipcoincombination.sh
+++ flipcoincombination.sh
@@ -1,12 +1,25 @@
 head=1
 tail=0
-
+head_count=0
+tail_count=0
+for(( i=1; i<=100;  i++))
+do
 result=$((RANDOM%2))
 
 if (($result==1))
 then
-echo "winner"=$head
-else  echo "losser"=$tail
+   ((head_count++))
+else
+   ((tail_count++))  
 fi
+done
+
+coin[winner]="head"
+coin[losser]="tail"
+
+echo ${coin[@]}
+
+echo "% of Head " =$head_count
+echo "% of tail " =$tail_count
 
 
