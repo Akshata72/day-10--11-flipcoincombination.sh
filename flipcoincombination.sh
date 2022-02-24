a0654f8fcd72a5652b7bbeae0d4de8011fd8a8df
Akshata72 <sableakshata880@gmail.com>
2022-02-24 20:24:00 +0530

    Added:UC4 logic

--- flipcoincombination.sh
+++ flipcoincombination.sh
@@ -1,9 +1,11 @@
 H=1
 T=0
-HHcount=0
-HTcount=0
-TTcount=0
-THcount=0
+HHHcount=0
+THHcount=0
+TTHcount=0
+TTTcount=0
+HTTcount=0
+HHTcount=0
 
 
 
@@ -13,34 +15,46 @@ for((i=1; i<=100; i++))
 do
    random_num=$((RANDOM%2))
    random_num2=$((RANDOM%2))
+   random_num3=$((RANDOM%2))
 
-if((random_num==1 && random_num2==1))
+if((random_num==1 && random_num2==1  &&  random_num3==1))
 then
-      echo "HH"
-       ((HHcount++))
-elif((random_num==1 && random_num2==0))
+      echo "HHH"
+       ((HHHcount++))
+elif((random_num==0 && random_num2==1 && random_num3==1 ))
 then
-      echo "HT"
-       ((HTcount++))
+      echo "THH"
+       ((THHcount++))
 
 
-elif((random_num==0 && random_num2==0))
+elif((random_num==0 && random_num2==0 &&  random_num3==1))
 then
-       echo "TT"
-          ((TTcount++))
+       echo "TTH"
+          ((TTHcount++))
 
+elif((random_num==0 && random_num2==0 &&  random_num3==0))
+then
+       echo "TTT"
+          ((TTTcount++))
+
+elif((random_num==1 && random_num2==0 &&  random_num3==0))
+then
+       echo "HTT"
+          ((HTTcount++))
 else
-       echo "TH"
-          ((THcount++))
+       echo "HHT"
+          ((HHTcount++))
 
 fi
 done
 
 
-echo "% of HH"= $HHcount "%"
-echo "% of HT"= $HTcount "%"
-echo "% of TT"= $TTcount "%"
-echo "% of TH"= $THcount "%"
+echo "% of HHH"= $HHHcount "%"
+echo "% of THH"= $THHcount "%"
+echo "% of TTH"= $TTHcount "%"
+echo "% of TTT"= $TTTcount "%"
+echo "% of HTT"= $HTTcount "%"
+echo "% of HHT"= $HHTcount "%"
 
 
 
